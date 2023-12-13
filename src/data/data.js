5363f5c92325b2a9f0e0828017d240f1057ae573
darshanhandi <darshanhandi.29@gmail.com>
2023-12-14 01:15:02 +0530

    Initial commit

--- src/data/data.js
+++ src/data/data.js
@@ -4,17 +4,17 @@ export const data = [
   {
     id: 1,
     img: 'https://i.postimg.cc/5t33FHYL/newspng.png',
-    title: "Multimedia Web",
+    title: "News 24*7",
     desc: "",
     link: "https://news244.netlify.app/",
     git: "https://github.com/darshanhandi/news_app.git",
   },
   {
     id: 2,
-    img: 'https://i.postimg.cc/Bv75CLyN/4167022.jpg',
-    title: "Tarvel Agency",
-    link: "",
-    git: "",
+    img: 'https://i.postimg.cc/qqHjFQMK/Screenshot-2023-12-08-221926.png',
+    title: "Ping-Pong Ball",
+    link: "https://pingpongballgame.netlify.app/",
+    git: "https://github.com/darshanhandi/ping-pong.git",
   },
   {
     id: 3,
